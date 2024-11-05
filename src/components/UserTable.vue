39b4e3c3288d2575fdb0841fa19d7c626d351b03
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2024-11-06 01:23:18 +0330

    feat: add UserTable component

---
+++ src/components/UserTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="column in columns" :key="column.key">{{ column.label }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.date }}</td>
+        <td>{{ user.address }}</td>
+        <td>{{ user.phone }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+import type { User } from '@/types/user'
+import usersData from '@/data/records.json'
+
+const users = ref<User[]>([...usersData])
+
+const columns = reactive([
+  {
+    key: 'id',
+    label: 'User ID',
+  },
+  {
+    key: 'name',
+    label: 'Name of the User',
+  },
+  {
+    key: 'date',
+    label: 'Date of Registration',
+  },
+  {
+    key: 'address',
+    label: 'Address',
+  },
+  {
+    key: 'phone',
+    label: 'Phone Number',
+  },
+])
+</script>
