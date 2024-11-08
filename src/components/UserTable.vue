1d89393a8cf415256309fc4f38c5ba217ced72e7
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2024-11-08 20:42:39 +0330

    feat: slightly clean up the table component with composables

--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -30,167 +30,53 @@
   <PaginationNavigator
     :current-page="currentPage"
     :total-pages="totalPages"
-    @on-paginate-click="setPage"
+    @on-paginate-click="setCurrentPage"
   />
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch, onBeforeMount } from 'vue'
+import { ref } from 'vue'
 import PageEntriesSelect from '@/components/PageEntriesSelect.vue'
 import PaginationNavigator from '@/components/PaginationNavigator.vue'
 
 import type { User } from '@/types/user'
-import { sortByDate, sortByName } from '@/helpers'
 import usersData from '@/data/records.json'
-import { useToggleColumnSort } from '@/composables/useToggleColumnSort'
-import { useRouter, useRoute } from 'vue-router'
+import { useURLSync } from '@/composables/useURLSync'
+import { useFilter } from '@/composables/useFilter'
+import { usePaginate } from '@/composables/usePaginate'
+import { useSort } from '@/composables/useSort'
 
 const users = ref<User[]>([...usersData])
-const router = useRouter()
-const route = useRoute()
-
-const { columnsSortDirection, toggleSort } = useToggleColumnSort()
-
-// sorting
-const nameColumnSortDirectionIndicator = computed(() => {
-  if (columnsSortDirection.value.name === 'asc') {
-    return '↑'
-  } else if (columnsSortDirection.value.name === 'desc') {
-    return '↓'
-  } else {
-    return null
-  }
-})
-
-const dateColumnSortDirectionIndicator = computed(() => {
-  if (columnsSortDirection.value.date === 'asc') {
-    return '↑'
-  } else if (columnsSortDirection.value.date === 'desc') {
-    return '↓'
-  } else {
-    return null
-  }
-})
-
-const sortedUsers = computed(() => {
-  if (columnsSortDirection.value.name !== 'idle' && columnsSortDirection.value.date !== 'idle') {
-    return [...filteredUsers.value].sort((a, b) => {
-      const nameComparison = sortByName(a, b, columnsSortDirection.value.name)
-      const dateComparison = sortByDate(a, b, columnsSortDirection.value.date)
-      return nameComparison !== 0 ? nameComparison : dateComparison
-    })
-  } else if (columnsSortDirection.value.name !== 'idle') {
-    return [...filteredUsers.value].sort((a, b) =>
-      sortByName(a, b, columnsSortDirection.value.name),
-    )
-  } else if (columnsSortDirection.value.date !== 'idle') {
-    return [...filteredUsers.value].sort((a, b) =>
-      sortByDate(a, b, columnsSortDirection.value.date),
-    )
-  } else {
-    return [...filteredUsers.value]
-  }
-})
 
 // filters
-const nameFilter = ref('')
-const phoneFilter = ref('')
-const addressFilter = ref('')
+const { addressFilter, filteredUsers, nameFilter, phoneFilter } = useFilter(users)
 
-const filteredUsers = computed(() => {
-  return users.value.filter(
-    (user) =>
-      user.name.toLowerCase().includes(nameFilter.value.toLowerCase()) &&
-      user.phone.toLowerCase().includes(phoneFilter.value.toLowerCase()) &&
-      user.address.toLowerCase().includes(addressFilter.value.toLowerCase()),
-  )
-})
+// sorting
+const {
+  sortedUsers,
+  dateColumnSortDirectionIndicator,
+  nameColumnSortDirectionIndicator,
+  columnsSortDirection,
+  toggleSort,
+} = useSort(filteredUsers)
 
 // paginating
+const {
+  pageEntriesOptions,
+  paginatedUsers,
+  setCurrentPage,
+  totalPages,
+  currentPage,
+  selectedPageEntries,
+} = usePaginate(sortedUsers)
 
-const selectedPageEntries = ref(10)
-const pageEntriesOptions = ref([
-  {
-    text: '5',
-    value: 5,
-  },
-  {
-    text: '10',
-    value: 10,
-  },
-  {
-    text: '15',
-    value: 15,
-  },
-  {
-    text: '20',
-    value: 20,
-  },
-  {
-    text: '50',
-    value: 50,
-  },
-])
-
-const currentPage = ref(1)
-const totalPages = computed(() => {
-  return Math.ceil(sortedUsers.value.length / selectedPageEntries.value)
-})
-const paginatedUsers = computed(() => {
-  const startIndex = (currentPage.value - 1) * selectedPageEntries.value
-  const endIndex = startIndex + selectedPageEntries.value
-  return sortedUsers.value.slice(startIndex, endIndex)
-})
-
-const setPage = (page: number) => {
-  currentPage.value = page
-}
-
-// url update
-function updateURL() {
-  router.push({
-    path: '',
-    query: {
-      ...route.query,
-      name: nameFilter.value,
-      phone: phoneFilter.value,
-      address: addressFilter.value,
-      page: currentPage.value,
-      pageCount: selectedPageEntries.value,
-      sort: JSON.stringify(columnsSortDirection.value),
-    },
-  })
-  console.log(columnsSortDirection.value)
-}
-
-// queries
-// sortBy, sortDir, name, address, phone, pageNumber, pageCount
-
-watch(
-  [
-    nameFilter,
-    phoneFilter,
-    addressFilter,
-    currentPage,
-    selectedPageEntries,
-    () => columnsSortDirection.value.name,
-    () => columnsSortDirection.value.date,
-  ],
-  updateURL,
+// url updates
+useURLSync(
+  nameFilter,
+  phoneFilter,
+  addressFilter,
+  currentPage,
+  selectedPageEntries,
+  columnsSortDirection,
 )
-
-function setupQueryValues() {
-  nameFilter.value = (route.query.name as string) ?? ''
-  phoneFilter.value = (route.query.phone as string) ?? ''
-  addressFilter.value = (route.query.address as string) ?? ''
-  currentPage.value = Number((route.query.page as string) ?? 1)
-  selectedPageEntries.value = Number((route.query.pageCount as string) ?? 10)
-  columnsSortDirection.value = JSON.parse(
-    (route.query.sort as string) ?? columnsSortDirection.value,
-  )
-}
-onBeforeMount(async () => {
-  await router.isReady()
-  setupQueryValues()
-})
 </script>
